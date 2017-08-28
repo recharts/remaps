98030a1c20a8663d7af71be5b1142cb6f46325d6
twobin <twobin@live.com>
2017-08-28 11:37:55 +0800

    fix:update index

--- src/index.js
+++ src/index.js
@@ -14,16 +14,14 @@ import Popup from './map/core/Popup';
 
 import ZoomControl from './map/core/ZoomControl';
 
-import GeoPath from './map/core/GeoPath';
+// import GeoPath from './map/core/GeoPath';
 
-import Projection from './map/core/Projection';
+// import Projection from './map/core/Projection';
 
-import Scale from './map/core/Scale';
+// import Scale from './map/core/Scale';
 
 import Legend from './map/core/Legend';
 
 export {
-  MapContainer, Maps, PolygonSet,
-
-  Popup, ZoomControl, GeoPath, Projection, Scale, Legend
+  MapContainer, Maps, PolygonSet, Popup, ZoomControl, Legend
 };
